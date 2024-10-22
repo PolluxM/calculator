445fbac9b5f3dbe2f35069e92e0e189fea39cf71
Rin <pelikhovmn@yandex.ru>
2024-10-22 19:29:06 +0300

    Создали 4 функции калькулятор, Функция деления с исключением деления на ноль

---
+++ M10_L1_L1.py
@@ -0,0 +1,16 @@
+# Калькулятор с графическим интерфейсом
+
+def add(a, b):
+    return a + b
+
+def subtruct(a, b):
+    return  a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Деление на ноль!"
